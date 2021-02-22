a4d1a8466ec264f9eb7888e140b3a7af64bba9c1
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-02-22 19:11:18 +0100

    Show diff+

--- src/ui.cljs
+++ src/ui.cljs
@@ -4,7 +4,8 @@
             [nubank.workspaces.card-types.react :refer [react-card]]
             [nubank.workspaces.core :refer [mount defcard]]
             [reagent.core :as r]
-            [clojure.tools.reader.edn :as edn]))
+            [clojure.tools.reader.edn :as edn]
+            [clojure.string :as str]))
 
 (defonce state (r/atom {}))
 
@@ -12,39 +13,45 @@
   [{:keys [map_1 map_2]}]
   (let [a_map (edn/read-string map_1)
         b_map (edn/read-string map_2)]
-    (when (and (map? a_map) (map? b_map))
-      (->>
-        (make a_map b_map)
-        (prepare-print  a_map)
-        str))))
+    (if (and (map? a_map) (map? b_map))
+      (let [diffs (->>
+                    (make a_map b_map)
+                    (prepare-print a_map))]
+        diffs)
+      "")))
 
 (defn diff
   [{:keys [map_1 map_2] :as state}]
-  [:div {:on-submit #()}
-   [:form
-    [:textarea
-     {:type :textarea
-      :rows 25
-      :columns 80
-      :auto-focus true
-      :default-value map_1
-      :on-change #(swap! state assoc :map_1 (-> % .-target .-value))}]
-    [:textarea
-     {:type :textarea
-      :rows 25
-      :columns 80
-      :default-value map_2
-      :on-change #(swap! state assoc :map_2 (-> % .-target .-value))}]
-    [:textarea
-     {:type     :textarea
-      :rows     25
-      :columns  80
-      :value    (try
-                  (update-diff @state)
-                  (catch :default e ""))
-      :readOnly true}]]])
+  [:div
+   [:textarea
+    {:type          :textarea
+     :rows          25
+     :columns       80
+     :auto-focus    true
 
-(defcard valami (react-card (r/as-element [diff state])))
+     :on-change     #(swap! state assoc :map_1 (-> % .-target .-value))}]
+   [:textarea
+    {:type          :textarea
+     :rows          25
+     :columns       80
+     :on-change     #(swap! state assoc :map_2 (-> % .-target .-value))}]
+   [:textarea
+    {:type     :textarea
+     :rows     25
+     :columns  80
+     :value    (try
+                 (str (update-diff @state))
+                 (catch :default e ""))
+     :readOnly true}]
+   [:div
+    (-> (try
+          (reduce (fn colorizer
+                    [acc [k v]]
+                    (concat acc [[:span {:style {:color :black}} k]
+                                 [:span {:style {:color :orange}} v]])) [] (update-diff @state))
+          (catch :default e "")))]])
+
+(defcard diff-card (react-card (r/as-element [diff state])))
 
 (defonce run (mount))
 
