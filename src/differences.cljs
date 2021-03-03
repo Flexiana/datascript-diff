d030db5079aebb96877f317ddfd2529df5af37d1
Ian Fernandez <d.ian.b@live.com>
2021-03-03 15:08:57 -0300

    Add commit-diff->have! fn to table

--- src/differences.cljs
+++ src/differences.cljs
@@ -32,9 +32,6 @@
            want-map]}]
   (map-diff have-map want-map))
 
-(defn map-diffs-ui [state]
-  [:table  {:style {:margin :auto
-                    :border "1px solid black"
 (defn commit-diff->have! [*state {:keys [path expected actual mismatch]}]
   (case mismatch
     :diff (swap! *state update :have-map
@@ -51,24 +48,28 @@
                         (-> %
                             :want-map
                             str))))
+
+(defn map-diffs-ui [*state]
+  [:table  {:style {:margin          :auto
+                    :border          "1px solid black"
                     :border-collapse "collapse"
-                    :text-align "center"}}
+                    :text-align      "center"}}
    [:thead [:tr {:style {:border "1px solid black"}}
             [:th {:style {:border "1px solid black"}}
              "Path"]
             [:th {:style {:border "1px solid black"}}
-             "Expected"]
-            [:th {:style {:border "1px solid black"}}
              "Actual"]
             [:th {:style {:border "1px solid black"}}
+             "Expected"]
+            [:th {:style {:border "1px solid black"}}
              "Mismatch"]]]
-   [:tbody (map (fn [{:keys [path expected actual mismatch]}]
+   [:tbody (map (fn [{:keys [path expected actual mismatch] :as d}]
                   (when-not (empty? path)
-                    [:tr {:key (apply str [path expected actual mismatch])
-                          :style {:border "1px solid black"
+                    [:tr {:key   (apply str [path expected actual mismatch])
+                          :style {:border           "1px solid black"
                                   :background-color (case mismatch
-                                                      :+ "rgba(51, 170, 51, .7)"
-                                                      :- "rgba(170, 51, 51, .7)"
+                                                      :+    "rgba(51, 170, 51, .7)"
+                                                      :-    "rgba(170, 51, 51, .7)"
                                                       :diff "rgba(254, 241, 96, 0.7)")}}
                      [:td {:style {:border "1px solid black"}}
                       (str path)]
@@ -77,44 +78,47 @@
                      [:td {:style {:border "1px solid black"}}
                       (str actual)]
                      [:td {:style {:border "1px solid black"}}
-                      (name mismatch)]]))
-                (map-diffs-from-editor state))]])
+                      (name mismatch)]
+                     [:td
+                      [:button {:on-click #(do (js/console.log [d *state])
+                                               (commit-diff->have! *state d)
+                                               (js/console.log [d *state]))}
+                       "Commit Diff"]]]))
+                (map-diffs-from-editor @*state))]])
 
 (defonce *editor (r/atom {}))
 
-(defn ui [state]
-  [:div {:style {:margin :auto
-                 :width  :max-content}}
-   [:table {:style {:margin :auto
-                    :width  :max-content}}
-    [:thead [:tr
-             [:th "Have Map"]
-             [:th "Want Map"]
-             [:th "Diffs"]]]
-    [:tbody [:tr
-             [:td [:label {:for "have-map"}
-                   [:textarea {:style     {:font-size "18pt"}
-                               :type      :textarea
-                               :rows      25
-                               :columns   80
-                               :on-change #(swap! state assoc :have-map (try
-                                                                          (edn/read-string
-                                                                           (.. %
-                                                                               -target
-                                                                               -value))
-                                                                          (catch js/Error e
-                                                                            nil)))}]]]
-             [:td [:label {:for "want-map"}
-                   [:textarea {:style     {:font-size "18pt"}
-                               :type      :textarea
-                               :rows      25
-                               :columns   80
-                               :on-change #(swap! state assoc :want-map (try
-                                                                          (edn/read-string
-                                                                           (.. %
-                                                                               -target
-                                                                               -value))
-                                                                          (catch js/Error e
-                                                                            nil)))}]]]
-             [:td [:label {:for "diffs"}
-                   [map-diffs-ui @state]]]]]]])
+(defn ui [*state]
+  (let [{:keys [have-map-input
+                want-map-input]} @*state]
+    [:div {:style {:margin :auto
+                   :width  :max-content}}
+     [:table {:style {:margin :auto
+                      :width  :max-content}}
+      [:thead [:tr
+               [:th "Have Map"]
+               [:th "Want Map"]
+               [:th "Diffs"]]]
+      [:tbody [:tr
+               [:td [:label {:for "have-map"}
+                     [:textarea {:style     {:font-size "18pt"}
+                                 :type      :textarea
+                                 :rows      25
+                                 :columns   80
+                                 :on-input #(let [input  (.. % -target -value)]
+                                              (swap! *state assoc
+                                                     :have-map-input input
+                                                     :have-map (input->map input)))
+                                 :value have-map-input}]]]
+               [:td [:label {:for "want-map"}
+                     [:textarea {:style     {:font-size "18pt"}
+                                 :type      :textarea
+                                 :rows      25
+                                 :columns   80
+                                 :on-input #(let [input  (.. % -target -value)]
+                                              (swap! *state assoc
+                                                     :want-map-input input
+                                                     :want-map (input->map input)))
+                                 :value want-map-input}]]]
+               [:td [:label {:for "diffs"}
+                     [map-diffs-ui *state]]]]]]]))
