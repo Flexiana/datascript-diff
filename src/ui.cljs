7864bfdf5c8facab677c4dfb45280fc481a98f2e
Ian Fernandez <d.ian.b@live.com>
2021-03-08 16:23:29 -0300

    Fix mount ui

--- src/ui.cljs
+++ src/ui.cljs
@@ -2,14 +2,14 @@
   (:require [reagent.dom :as dom]
             [nubank.workspaces.card-types.react :refer [react-card]]
             [nubank.workspaces.core :refer [mount defcard]]
-            [diff-ui :refer [diff state]]
+            [diff-ui :refer [diff-ui state]]
             [differences :as difference]
             [reagent.core :as r]))
 
-(defcard diff-card
-  (react-card (r/as-element [diff state])))
+(defcard diff-card (react-card (r/as-element [diff-ui state])))
 
 (defcard differences-card
   (react-card (r/as-element [difference/ui difference/*editor])))
 
-(defonce run (mount))
+(defonce run
+  (mount))
