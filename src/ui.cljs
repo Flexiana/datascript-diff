2caa61a672396692baa7f5986e1f4adf8f7df6e5
Ian Fernandez <d.ian.b@live.com>
2021-03-09 04:55:25 -0300

    Add flex align

--- src/ui.cljs
+++ src/ui.cljs
@@ -2,6 +2,7 @@
   (:require [reagent.dom :as dom]
             [nubank.workspaces.card-types.react :refer [react-card]]
             [nubank.workspaces.core :refer [mount defcard]]
+            [nubank.workspaces.model :as wsm]
             [diff-ui :refer [diff-ui state]]
             [differences :as difference]
             [reagent.core :as r]))
@@ -9,6 +10,7 @@
 (defcard diff-card (react-card (r/as-element [diff-ui state])))
 
 (defcard differences-card
+  {::wsm/align {:flex 1}}
   (react-card (r/as-element [difference/ui difference/*editor])))
 
 (defonce run
